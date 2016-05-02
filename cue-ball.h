8d712b13905f6a88f4c7af89837862581f59e79a
Frances Tso <francest1995@hotmail.com>
2016-05-01 21:18:13 -0400

    Delete cue-ball.h

--- cue-ball.h
+++
@@ -1,8 +0,0 @@
-#include "vec3f.h"
-
-double calcForce(double V0);
-Vec3f linearVelocity(double F);
-Vec3f angularVelocity(double F);
-Vec3f flipYZ(Vec3f v);
-Vec3f rotateToCue(Vec3f pos, Vec3f cue, Vec3f pt);
-Vec3f matrixMult(Vec3f* R, Vec3f pos);
