35b2020578761b58b7bc43764d6b6a468e621b08
Frances Tso <francest1995@hotmail.com>
2016-05-01 21:19:55 -0400

    Delete rolling.h

--- rolling.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-Vec3f positionR(double t, Vec3f initPos, int collidedRail, double V0);
-Vec3f linearVelocityR(double t, double V0);
-
-double angularVelocityR(double t, double V0);
-double rollDuration(Vec3f initV);
